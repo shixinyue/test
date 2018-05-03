63e68d1c41a1a1896a85c69e49a45a1f74e6c98d
shixinyue <779318986@qq.com>
2018-05-03 18:05:45 +0800

    提交代码

---
+++ maven-hello-world/src/test/java/hello_world/AppTest.java
@@ -0,0 +1,20 @@
+package hello_world;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
